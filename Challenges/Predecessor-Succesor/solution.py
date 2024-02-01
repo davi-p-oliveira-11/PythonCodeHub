17bec07a55d7f442a6ee75d469d7be731fb4a28d
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-02-01 12:15:56 -0300

    Create solution.py

---
+++ Challenges/Predecessor-Succesor/solution.py
@@ -0,0 +1,12 @@
+def sucessor_predecessor():
+    number = int(input('Enter a number: '))
+
+    predecessor = number - 1
+    successor = number + 1
+
+    print(f'The predecessor of {number} is {predecessor}')
+    print(f'The successor of {number} is {successor}')
+
+
+sucessor_predecessor()
+input("Press Enter to Exit ... ")
