71f6a5b6768891256a07b82060108f9d2a55a244
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-02-01 02:19:41 -0300

    Create solution.py

---
+++ Challenges/SumOfTwo/solution.py
@@ -0,0 +1,12 @@
+def sum_of_two():
+    num_1 = int(input('Enter a number: '))
+    num_2 = int(input('Enter another number: '))
+
+    sum = num_1 + num_2
+
+
+    print(f'The sum between {num_1} and {num_2} is {sum}')
+
+
+sum_of_two()
+input("Press Enter to Exit ... ")
