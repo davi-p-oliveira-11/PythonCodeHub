e497cadf166f00932c9a18253d09e762aa93f88d
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-02-01 02:53:00 -0300

    Create solution.py

---
+++ Challenges/AverageOfTwo/solution.py
@@ -0,0 +1,9 @@
+def average_of_two():
+    grade_1 = float(input('Enter the first grade: '))
+    grade_2 = float(input('Enter the second grade: '))
+
+    average = (grade_1 + grade_2) / 2
+
+    print(f'The average between {grade_1} and {grade_2} is equal to {average}')
+
+average_of_two()
