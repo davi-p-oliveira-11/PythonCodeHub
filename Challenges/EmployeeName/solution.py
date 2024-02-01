acef8af58ade7e592ce494e89c0e76bd7df9a2d3
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-02-01 02:07:45 -0300

    Create solution.py

---
+++ Challenges/EmployeeName/solution.py
@@ -0,0 +1,9 @@
+def say_name():
+    employee_name = input('What is the employee name ? ')
+    employee_salary = input('What is the value of the employee salary ? ')
+    
+    print(f'Employee {employee_name} has a salary of {employee_salary} USD')
+
+
+say_name()
+input("Press Enter to Exit ... ")
