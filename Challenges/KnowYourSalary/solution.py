945fcf7a712c150e1cc4c42f342672404e127dad
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-02-03 17:52:06 -0300

    Create solution.py

---
+++ Challenges/KnowYourSalary/solution.py
@@ -0,0 +1,11 @@
+def know_your_salary():
+  days_worked = int(input("How many days did you work last month ? "))
+  day_of_work = 200
+  employee_salary = days_worked * day_of_work
+
+  print(f'The employee worked for {days_worked} days and will 
+  receive a salary of {employee_salary} USD.')
+
+
+know_your_salary()
+print("Press Enter to Exit ... ")
